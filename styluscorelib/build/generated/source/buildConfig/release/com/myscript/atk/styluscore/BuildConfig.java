e4cb79f4439bd15d21503c5d1a13f469df6ff747
bikevin <kevin.bi.1002@gmail.com>
2016-02-27 11:55:25 -0500

    more libs

---
+++ styluscorelib/build/generated/source/buildConfig/release/com/myscript/atk/styluscore/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.myscript.atk.styluscore;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.myscript.atk.styluscore";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
